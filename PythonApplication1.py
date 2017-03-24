775c08be6f9b657cc6da192d95f56a85698dd3f1
masrin abd rahman <masrin.abr@outlook.com>
2017-03-25 05:47:33 +0800

    adding main files

---
+++ PythonApplication1.py
@@ -0,0 +1,18 @@
+from kivy.app import App
+#kivy.require("1.9.0")
+from kivy.uix.floatlayout import FloatLayout
+from kivy.uix.widget import Widget
+
+
+class mainlayout(Widget):
+    pass
+
+class SimpleKivy4(App):
+    def build(self):
+        return mainlayout()
+
+if __name__ == "__main__":
+    SimpleKivy4().run()
+#add something
+
+
